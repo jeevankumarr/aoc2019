4a63cdd64e899f47502dc7f347f598aef87e1418
Jeevan Kumar <jeevs_kr@protonmail.coma>
2019-12-02 22:20:27 -0800

    refactored code

--- 1002_computer.py
+++ 1002_computer.py
@@ -3,51 +3,36 @@ def compute(inputfile, a=12, b=2):
     data_raw = open(inputfile).read()
     data = data_raw.split(',')
     data = [int(x) for x in data]
-    # print(data)
-    mem = dict()
 
-
-    for i, d in enumerate(data):
-        mem[i] = d
-    # print(f'14 {mem[14]}')
-    mem[1] = a
-    mem[2] = b
+    data[1] = a
+    data[2] = b
     i = 0
+
     while i < len(data):
-        if mem[i] == 99:
-            i += 1
+        if data[i] == 99:
             break
-        if mem[i] == 1 or mem[i] == 2:
-            l = mem[i + 1]
-            r = mem[i + 2]
-            dest = mem[i + 3]
-            try:
-                # print(f'Pr {i}/{mem[i]}: {l} {r} -> {dest} [ {mem[dest]} <- {mem[l]} {"+" if mem[i] == 1 else "*"} {mem[r]}]')
-                if mem[i] == 1:
+        l = data[i + 1]
+        r = data[i + 2]
+        dest = data[i + 3]
+        # print(f'Pr {i}/{mem[i]}: {l} {r} -> {dest} [ {mem[dest]} <- {mem[l]} {"+" if mem[i] == 1 else "*"} {mem[r]}]')
+        if data[i] == 1:
+            data[dest] = data[l] + data[r]
+        elif data[i] == 2:
+            data[dest] = data[l] * data[r]
 
-                    mem[dest] = mem[l] + mem[r]
+        # print(f'Po {i}/{mem[i]}: {l} {r} -> {dest} [ {mem[dest]} <- {mem[l]} {"+" if mem[i] == 1 else "*"} {mem[r]}]')
+        i += 4
 
-                elif mem[i] == 2:
-                    mem[dest] = mem[l] * mem[r]
-                # print(f'Po {i}/{mem[i]}: {l} {r} -> {dest} [ {mem[dest]} <- {mem[l]} {"+" if mem[i] == 1 else "*"} {mem[r]}]')
-                # print(f'Success {mem[dest]}[{dest}] <- {mem[l]}[{l}] {"+" if mem[i] == 1 else "*"} {mem[r]}[{r}]')
-                # print(f'Success {i}: Op: {data[i]}, l = {data[i + 1]} meml = {mem[l]}, r = {data[i + 2]} memr = {mem[r]}=> {data[i + 3]}, mem={mem}')
-                i += 4
 
-            except Exception as e:
-                print(f'Err {i}: {mem[i]}, {mem[i+1]}, {mem[i+2]} => {mem[i+3]}, mem = {mem}')
-                traceback.print_exc()
-                raise
-        else:
-            print(f'Err {i}')
-    return mem[0]
-def run():
-    result = 19690720
+
+    return data[0]
+def run(result=19690720):
+
     for i in range(0, 99):
         for j in range(0, 99):
             if result == compute('./data/1002_computer.data', i, j):
                 print(i, j)
                 break
 if __name__ == '__main__':
-    # compute('./data/1002_computer.data')
-    run()
+    # print(compute('./data/1002_computer.data'))
+    run(19690720)
